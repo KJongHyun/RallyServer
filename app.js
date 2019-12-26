21a8bb7cbb93c37947515ebe9ae44b4416b19caf
jonghyeonkim <whdgus8219@naver.com>
2019-12-27 03:14:10 +0900

    새벽 커밋

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const database = require('mysql');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
@@ -5,6 +6,16 @@ app.use(express.json());
 
 const hostName = '127.0.0.1';
 const port = 3000;
+
+const connection = database.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'rally'
+});
+
+connection.connect();
 
 app.route('/login').post(async (res, req) => {
 
@@ -15,6 +26,8 @@ app.route('/excerData').get(async (res, req) => {
 });
 
 app.route('/addExcerInfo').post(async (res, req) => {
+    //docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -d -p 3306:3306 mysql
+
 
 });
 
@@ -31,5 +44,5 @@ app.route('/removeExcerInfo').post(async (res, req) => {
 });
 
 app.listen(port, hostName, () => {
-console.log(`Server is running at https://${hostName}:${port}`);
+    console.log(`Server is running at https://${hostName}:${port}`);
 }); 
