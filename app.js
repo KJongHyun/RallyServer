9508f033f5dabe334a89976be24190d0c2561345
jonghyeonkim <whdgus8219@naver.com>
2019-12-26 18:20:44 +0900

    Function 추가

--- app.js
+++ app.js
@@ -6,7 +6,29 @@ app.use(express.json());
 const hostName = '127.0.0.1';
 const port = 3000;
 
+app.route('/login').post(async (res, req) => {
 
+});
+
+app.route('/excerData').get(async (res, req) => {
+
+});
+
+app.route('/addExcerInfo').post(async (res, req) => {
+
+});
+
+app.route('/getExcerInfo').get(async (res, req) => {
+
+});
+
+app.route('/updateExcerInfo').post(async (res, req) => {
+
+});
+
+app.route('/removeExcerInfo').post(async (res, req) => {
+
+});
 
 app.listen(port, hostName, () => {
 console.log(`Server is running at https://${hostName}:${port}`);
