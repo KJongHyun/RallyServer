8ed3261b74b9db43d5d17aaf4c047d1ae9f0575c
jonghyeonkim <whdgus8219@naver.com>
2019-12-26 18:00:31 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+const hostName = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostName, () => {
+console.log(`Server is running at https://${hostName}:${port}`);
+});
