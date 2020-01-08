b99ee0237cacda6ee299b620a82f783f956a7a8f
jonghyeonkim <whdgus8219@naver.com>
2020-01-08 23:40:32 +0900

    커밋

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     userId: DataTypes.STRING,
-    userName: DataTypes.STRING,
+    userName: DataTypes.STRING, 
     pw: DataTypes.STRING
   }, {});
   user.associate = function(models) {
