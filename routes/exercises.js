bd06330403e54758ac44bb265797997c84e088db
jonghyeonkim <whdgus8219@naver.com>
2020-01-13 23:27:11 +0900

    20200113-exercise 메소드 추가

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const models = require("../models");
 
-router.post("/create", function (req, res) {
+router.post("/create", async function (req, res) {
+    const userId = req.body.userId;
+    const exerciseName = req.body.exerciseName;
+
+    const newExercise = {
+        userId: userId,
+        exerciseName: exerciseName
+    }
+
+    const exercise = await models.exercise.create(newExercise);
     
 });
 
-router.route('/addExcerInfo').post(async (res, req) => {
+router.route('/addExcerInfo').post(async (req, res) => {
     //docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -d -p 3306:3306 mysql
 
 
 });
 
-router.route('/getExcerInfo').get(async (res, req) => {
+router.route('/getExcerInfo').get(async (req, res) => {
+    const id = req.param("id");
+    const exercise = models.exercise.findOne({
+        where:{id:id}
+    });
 
+    res.end(JSON.stringify(exercise));
 });
 
 router.route('/updateExcerInfo').post(async (res, req) => {
